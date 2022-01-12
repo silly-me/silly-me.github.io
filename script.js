7798cfba9bde344d8f03e342500219f49919a637
silly-me <97390859+silly-me@users.noreply.github.com>
2022-01-12 23:08:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,166 @@
-var imageLoader = document.getElementById('imageLoader');
-    imageLoader.addEventListener('change', handleImage, false);
-var canvas = document.getElementById('imageCanvas');
-var ctx = canvas.getContext('2d');
-var messageInput = document.getElementById('message');
+$('button.encode, button.decode').click(function(event) {
+  event.preventDefault();
+});
 
-var textCanvas = document.getElementById('textCanvas');
-var tctx = textCanvas.getContext('2d');
+function previewDecodeImage() {
+  var file = document.querySelector('input[name=decodeFile]').files[0];
 
-//handle decoding
-var decodeCanvas = document.getElementById('imageCanvas2');
-var dctx = decodeCanvas.getContext('2d');
-var imageLoader2 = document.getElementById('imageLoader2');
-    imageLoader2.addEventListener('change', handleImage2, false);
-
-function handleImage(e){
-    var reader = new FileReader();
-    reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
-            canvas.width = img.width;
-            canvas.height = img.height;
-            textCanvas.width=img.width;
-            textCanvas.height=img.height;
-            tctx.font = "30px Arial";
-      var messageText = (messageInput.value.length) ? messageInput.value : 'Hello';
-            tctx.fillText(messageText,10,50);
-            ctx.drawImage(img,0,0);
-            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            var textData = tctx.getImageData(0, 0, canvas.width, canvas.height);
-            var pixelsInMsg = 0;
-                pixelsOutMsg = 0;
-            for (var i = 0; i < textData.data.length; i += 4) {
-                if (textData.data[i+3] !== 0) {
-                    if (imgData.data[i+1]%10 == 7) {
-                        //do nothing, we're good
-                    }
-                    else if (imgData.data[i+1] > 247) {
-                        imgData.data[i+1] = 247;
-                    }
-                    else {
-                        while (imgData.data[i+1] % 10 != 7) {
-                            imgData.data[i+1]++;
-                        }
-                    }
-                    pixelsInMsg++;
-                }
-                else {
-                    if (imgData.data[i+1]%10 == 7) {
-                        imgData.data[i+1]--;
-                    }
-                    pixelsOutMsg++;
-                }
-            }
-            console.log('pixels within message borders: '+pixelsInMsg);
-            console.log('pixels outside of message borders: '+pixelsOutMsg);
-            ctx.putImageData(imgData, 0, 0);
-        };
-        img.src = event.target.result;
-    };
-    reader.readAsDataURL(e.target.files[0]);
+  previewImage(file, ".decode canvas", function() {
+    $(".decode").fadeIn();
+  });
 }
 
-function handleImage2(e){
-    console.log('handle image 2');
-    var reader2 = new FileReader();
-    reader2.onload = function(event){
-        console.log('reader2 loaded');
-        var img2 = new Image();
-        img2.onload = function(){
-            console.log('img2 loaded');
-            decodeCanvas.width = img2.width;
-            decodeCanvas.height = img2.height;
-            dctx.drawImage(img2,0,0);
-            var decodeData = dctx.getImageData(0, 0, decodeCanvas.width, decodeCanvas.height);
-            for (var i = 0; i < decodeData.data.length; i += 4) {
-                if (decodeData.data[i+1] % 10 == 7) {
-                    decodeData.data[i] = 0;
-                    decodeData.data[i+1] = 0;
-                    decodeData.data[i+2] = 0;
-                    decodeData.data[i+3] = 255;
-                }
-                else {
-                    decodeData.data[i+3] = 0;
-                }
-            }
-            dctx.putImageData(decodeData, 0, 0);
-        };
-        img2.src = event.target.result;
-    };
-    reader2.readAsDataURL(e.target.files[0]);
+function previewEncodeImage() {
+  var file = document.querySelector("input[name=baseFile]").files[0];
+
+  $(".images .nulled").hide();
+  $(".images .message").hide();
+
+  previewImage(file, ".original canvas", function() {
+    $(".images .original").fadeIn();
+    $(".images").fadeIn();
+  });
 }
+
+function previewImage(file, canvasSelector, callback) {
+  var reader = new FileReader();
+  var image = new Image;
+  var $canvas = $(canvasSelector);
+  var context = $canvas[0].getContext('2d');
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+
+  reader.onloadend = function () {
+    image.src = URL.createObjectURL(file);
+
+    image.onload = function() {
+      $canvas.prop({
+        'width': image.width,
+        'height': image.height
+      });
+
+      context.drawImage(image, 0, 0);
+
+      callback();
+    }
+  }
+}
+
+function encodeMessage() {
+  $(".error").hide();
+  $(".binary").hide();
+
+  var text = $("textarea.message").val();
+
+  var $originalCanvas = $('.original canvas');
+  var $nulledCanvas = $('.nulled canvas');
+  var $messageCanvas = $('.message canvas');
+
+  var originalContext = $originalCanvas[0].getContext("2d");
+  var nulledContext = $nulledCanvas[0].getContext("2d");
+  var messageContext = $messageCanvas[0].getContext("2d");
+
+  var width = $originalCanvas[0].width;
+  var height = $originalCanvas[0].height;
+
+  // Check if the image is big enough to hide the message
+  if ((text.length * 8) > (width * height * 3)) {
+    $(".error")
+      .text("Text too long for chosen image....")
+      .fadeIn();
+
+    return;
+  }
+
+  $nulledCanvas.prop({
+    'width': width,
+    'height': height
+  });
+
+  $messageCanvas.prop({
+    'width': width,
+    'height': height
+  });
+
+  // Normalize the original image and draw it
+  var original = originalContext.getImageData(0, 0, width, height);
+  var pixel = original.data;
+  for (var i = 0, n = pixel.length; i < n; i += 4) {
+    for (var offset =0; offset < 3; offset ++) {
+      if(pixel[i + offset] %2 != 0) {
+        pixel[i + offset]--;
+      }
+    }
+  }
+  nulledContext.putImageData(original, 0, 0);
+
+  // Convert the message to a binary string
+  var binaryMessage = "";
+  for (i = 0; i < text.length; i++) {
+    var binaryChar = text[i].charCodeAt(0).toString(2);
+
+    // Pad with 0 until the binaryChar has a lenght of 8 (1 Byte)
+    while(binaryChar.length < 8) {
+      binaryChar = "0" + binaryChar;
+    }
+
+    binaryMessage += binaryChar;
+  }
+  $('.binary textarea').text(binaryMessage);
+
+  // Apply the binary string to the image and draw it
+  var message = nulledContext.getImageData(0, 0, width, height);
+  pixel = message.data;
+  counter = 0;
+  for (var i = 0, n = pixel.length; i < n; i += 4) {
+    for (var offset =0; offset < 3; offset ++) {
+      if (counter < binaryMessage.length) {
+        pixel[i + offset] += parseInt(binaryMessage[counter]);
+        counter++;
+      }
+      else {
+        break;
+      }
+    }
+  }
+  messageContext.putImageData(message, 0, 0);
+
+  $(".binary").fadeIn();
+  $(".images .nulled").fadeIn();
+  $(".images .message").fadeIn();
+};
+
+function decodeMessage() {
+  var $originalCanvas = $('.decode canvas');
+  var originalContext = $originalCanvas[0].getContext("2d");
+
+  var original = originalContext.getImageData(0, 0, $originalCanvas.width(), $originalCanvas.height());
+  var binaryMessage = "";
+  var pixel = original.data;
+  for (var i = 0, n = pixel.length; i < n; i += 4) {
+    for (var offset =0; offset < 3; offset ++) {
+      var value = 0;
+      if(pixel[i + offset] %2 != 0) {
+        value = 1;
+      }
+
+      binaryMessage += value;
+    }
+  }
+
+  var output = "";
+  for (var i = 0; i < binaryMessage.length; i += 8) {
+    var c = 0;
+    for (var j = 0; j < 8; j++) {
+      c <<= 1;
+      c |= parseInt(binaryMessage[i + j]);
+    }
+
+    output += String.fromCharCode(c);
+  }
+
+  $('.binary-decode textarea').text(output);
+  $('.binary-decode').fadeIn();
+};
